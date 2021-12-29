a86e306cfbecf078b9a9556bb7d33c2ea4d42cc1
rosental <cristiane.rosental@gmail.com>
2021-12-28 23:46:17 -0300

    SQL Controller.

--- src/Controller/SqlController.php
+++ src/Controller/SqlController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
 use App\Repository\EntityRepository\AbstractEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SqlController extends AbstractController
 {
@@ -33,5 +37,25 @@ class SqlController extends AbstractController
     {
         $callFirstInsert = $this->abstractEntityRepository->callProcedureFirstInsert();
         return new JsonResponse(["result" => $callFirstInsert], 200);
+    }
+
+    /**
+     * @Route("/first/user", name="get_first_user")
+     */
+    public function firstUser( EntityManagerInterface $em,UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $user->setName("Cristiane Rosental");
+        $user->setUsername("Rosental");
+        $user->setEmail("cristiane.rosental@gmail.com");
+        $password = $passwordEncoder->encodePassword($user, "123456");
+        $user->setPassword($password);
+        $user->setImage('23d38a7a67e705b154f803df50f58f08.jpg');
+        $user->setRoles("ROLE_ADMIN");
+
+        $em->persist($user);
+        $em->flush();
+
+        return $this->redirectToRoute('login');
     }
 }
